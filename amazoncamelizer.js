77750adbac60c3288d3f9f808a4dfebfba7784ba
maxwroc <max.chodorowski@gmail.com>
2017-12-05 21:18:39 +0000

    Amazon Camelizer

---
+++ amazoncamelizer.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name           AmazonCamelizer: CamelCamelCamel Graphs
+// @version        0.1
+// @description    Adds CamelCamelCamel graph to all Amazon product pages.
+// @author         maxwroc
+// @namespace      https://github.com/maxwroc/UserScripts
+// @include        /^https?://www\.amazon\.(com|co\.uk|ca|de|fr|es|it|cn|co\.jp)/*/
+// @noframes
+// ==/UserScript==
+
+(() => {
+    class AmazonCamelizer {
+        constructor() {
+            let element = document.querySelector('input[id="ASIN"]');
+            this.asin = element.value.replace(/\s/g, "");
+
+            this.country = location.hostname.split(".").pop();
+            if (this.country == "com") {
+                this.country = "us";
+            }
+
+            if (!this.country || !this.asin) {
+                throw `Failed to get country or asin values`;
+            }
+        }
+
+        getCamelUrl() {
+            return `http://${this.country}.camelcamelcamel.com/product/${this.asin}`;
+        }
+
+        getCamelImageUrl() {
+            return `http://charts.camelcamelcamel.com/${this.country}/${this.asin}/amazon-new.png?force=1&zero=0&w=${width}&h=${height}&desired=false&legend=1&ilt=1&tp=all&fo=0`;
+        }
+
+        insertGraphBefore(elemSelector, width = 500, height = 250) {
+            let elem = document.querySelector(elemSelector);
+            if (!elem) {
+                throw "Element not found: " + elemSelector;
+            }
+
+            let container = document.createElement("div");
+            container.innerHTML = `<hr /><a href="${this.getCamelUrl()}" target="_blank"><img src="${this.getCamelImageUrl()}" style="margin-bottom: 15px" /></a><hr />`;
+            elem.parentElement.insertBefore(container, elem);
+        }
+    }
+
+    try {
+        let a = new AmazonCamelizer();
+        a.insertGraphBefore("#price_feature_div+*");
+    }
+    catch (e) {
+        console.warn("[AmazonCamelizer]: " + e.message);
+    }
+})();
