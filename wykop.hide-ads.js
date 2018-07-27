d69d34a4769330ae038446150d89613b413e0c84
Max Chodorowski <max.chodorowski@gmail.com>
2018-07-27 22:35:57 +0100

    Revert removed prev ver of logic - still needed

--- wykop.hide-ads.js
+++ wykop.hide-ads.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Wykop.pl - hide sponsored articles
 // @namespace    https://github.com/maxwroc/UserScripts
-// @version      0.2
+// @version      0.3
 // @description  Skrypt do ukrywania sponsorowanych artykulow
 // @author       maxwroc
 // @match        https://www.wykop.pl/*
 // ==/UserScript==
 
 (function() {
+  var s = document.createElement("style");
+  s.type = "text/css";
+  s.textContent = ".link.iC .article:not(.dC) { display: none; }";
+  document.head.appendChild(s);
+    
   $(".article .diggbox a[title='dodaj jako znalezisko']").closest("li").attr("style", "display: none");
 })();
